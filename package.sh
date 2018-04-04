1bcf4c83b92816341f0270f56faa39f87d02d793
Mike Levine <m.levine@f5.com>
2018-04-04 15:22:40 -0700

    Remove peer dependency before packaging

--- package.sh
+++ package.sh
@@ -4,4 +4,8 @@ if [[ $1 == '--no-deps' ]]; then
     npm install --production
 fi
 
+# We have an old version of npm on the build machines which installs peer dependencies.
+# We do not want this/
+rm -rf node_modules/@f5devcentral
+
 tar -C .. --exclude=".git*" --exclude="test" --exclude="${PWD##*/}/dist" --exclude="doc" --exclude="${PWD##*/}/.vscode" -zcf dist/f5-cloud-libs-openstack.tar.gz f5-cloud-libs-openstack
